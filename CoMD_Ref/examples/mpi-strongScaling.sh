ba69be91a97ec5c549baa0aa3bcff44364a9044b
Cristian Arias <cristian.arias.chaves@hotmail.com>
2024-02-24 21:04:48 -0600

    Update examples files

--- CoMD_Ref/examples/mpi-strongScaling.sh
+++ CoMD_Ref/examples/mpi-strongScaling.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Simple strong scaling study with eam potential and 256,000 atoms
-mpirun -np 1 -hostfile /nfs/hosts.openmpi  ../bin/CoMD-mpi -e -i 1 -j 1 -k 1 -x 40 -y 40 -z 40
-mpirun -np 2 -hostfile /nfs/hosts.openmpi  ../bin/CoMD-mpi -e -i 2 -j 1 -k 1 -x 40 -y 40 -z 40
-mpirun -np 4 -hostfile /nfs/hosts.openmpi  ../bin/CoMD-mpi -e -i 2 -j 2 -k 1 -x 40 -y 40 -z 40
-mpirun -np 8 -hostfile /nfs/hosts.openmpi  ../bin/CoMD-mpi -e -i 2 -j 2 -k 2 -x 40 -y 40 -z 40
-mpirun -np 16 -hostfile /nfs/hosts.openmpi ../bin/CoMD-mpi -e -i 4 -j 2 -k 2 -x 40 -y 40 -z 40
+mpirun -np 1 /nfs/mantevo-benchmarks/CoMD_Ref/bin/CoMD-mpi -e -i 1 -j 1 -k 1 -x 40 -y 40 -z 40
+mpirun -np 2 /nfs/mantevo-benchmarks/CoMD_Ref/bin/CoMD-mpi -e -i 2 -j 1 -k 1 -x 40 -y 40 -z 40
+mpirun -np 4 /nfs/mantevo-benchmarks/CoMD_Ref/bin/CoMD-mpi -e -i 2 -j 2 -k 1 -x 40 -y 40 -z 40
+mpirun -np 8 /nfs/mantevo-benchmarks/CoMD_Ref/bin/CoMD-mpi -e -i 2 -j 2 -k 2 -x 40 -y 40 -z 40
+mpirun -np 16 /nfs/mantevo-benchmarks/CoMD_Ref/bin/CoMD-mpi -e -i 4 -j 2 -k 2 -x 40 -y 40 -z 40
